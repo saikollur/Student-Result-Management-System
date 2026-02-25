f800318e6c821c6bd138e396b0a252594baca235
saikollur <saiajju14@gmail.com>
2026-02-25 22:57:18 +0530

    added crud operations : add, read, search

--- app.py
+++ app.py
@@ -1,23 +1,59 @@
-from flask import Flask
-from database import init_db
+from flask import Flask, request, redirect, url_for, render_template
+from database import init_db, add_student, get_students, search_student
 
 app = Flask(__name__)
 
+#whenever we restart this gives the webserver context
 with app.app_context():
     init_db()
 
 # this is a decorator
 @app.route('/')
 def home():
-    return 'Student Result Management System'
+    return render_template('index.html')
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_student():
-    return 'This is the Add Student page'
+    message = ''
+    if request.method == 'POST':
+        roll_no = request.form['roll_no'].strip()
+        name = request.form['name'].strip()
+        marks = request.form['marks'].strip()
+        grade = request.form['grade'].strip()
+
+        if not roll_no or not name or not marks or not grade:
+            message = 'All fields are required!'
+        elif not marks.isdigit() or not (0 <= int(marks) <= 100):
+            message = 'Marks must be a number between 0 and 100!'
+        else :
+            try:
+                add_student(roll_no, name, int(marks), grade)
+                message = 'Student added successfully!'
+            except Exception as e:
+                message = f'Error: Roll number already exists'
+    return render_template('add_student.html', message = message)
 
 @app.route('/view')
-def view_student():
-    return 'This is the View Student page'
+def view_students():
+    students = get_all_students()
+    return render_template('view_students.html', students = students)
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    student = None
+    message = ''
+
+    if request.methods == 'POST':
+        roll_no = request.form['roll_no'].strip()
+
+        if not roll_no:
+            message = 'Please enter a roll number!'
+        else:
+            student = search_student(roll_no)
+            if not student:
+                message = f'No student found with Roll No: {roll_no}'
+
+    return render_template('search.html', message=message)
+    
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -2,11 +2,13 @@ import sqlite3
 
 def get_connection():
     conn = sqlite3.connect('student.db')
+    #usually sql queries return in tuples, this makes them to return as a dictionary
     conn.row_factory = sqlite3.Row
     return conn
 
 def init_db():
     conn = get_connection()
+    #cursor is used to run sql statements
     cursor = conn.cursor()
 
     cursor.execute('''
@@ -19,5 +21,41 @@ def init_db():
         )
     ''')
 
+    #saves changes to db
     conn.commit()
     conn.close()
+
+def add_student(roll_no, name, marks, grade):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO students (roll_no, name, marks, grade)
+        VALUES (?, ?, ?, ?)
+    ''', (roll_no, name, marks, grade))
+
+    conn.commit()
+    conn.close()
+
+def get_students():
+    conn = get_connected()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM Students')
+    #collects all the results into a list.
+    students = cursor.fetchall()
+
+    conn.close()
+    return students
+
+def search_student(roll_no):
+    conn = get_connection
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM students WHERE roll_no = ?', (roll_no,))
+    student = cursor.fetchone
+
+    conn.close()
+    return student
+
+
